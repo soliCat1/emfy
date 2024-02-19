e9f52ec9b50c7f995f32510f5e9009f00dd9759e
DepreKotik <vovachegotov@gmail.com>
2024-02-19 19:12:33 +0300

    ++

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,25 @@
 
 <script>
 
+
 export default {
   name: "App",
   data: () => ({
     deals: []
   }),
   mounted() {
-    fetch("https://vovachegotov.amocrm.ru/api/v4/leads",{
-      mode: 'no-cors',
-      headers: {
-        'Authorization: Bearer': "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijk2NDFjNzNkYWFmOWQzZDdkODBjNDIwZDNlOGZhYzBhNTk0Y2M2OGFjOTY5ODBkOWE4NDMyZGE2OWMxNGEzMTMzNjUwMWUwNmM4ODcxOWQxIn0.eyJhdWQiOiI0ZmE3M2VhNi1jMDdlLTQ3OTEtODk4MC1iNzYxNjVmODkyMGMiLCJqdGkiOiI5NjQxYzczZGFhZjlkM2Q3ZDgwYzQyMGQzZThmYWMwYTU5NGNjNjhhYzk2OTgwZDlhODQzMmRhNjljMTRhMzEzMzY1MDFlMDZjODg3MTlkMSIsImlhdCI6MTcwODAyMjkwMCwibmJmIjoxNzA4MDIyOTAwLCJleHAiOjE3MDgxMDkzMDAsInN1YiI6IjEwNjc5NDk0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNTcxODE0LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMzdiM2I3OGMtNTRkNi00ZjI1LWE4YjktZjQ2NTcwMWU0ZDYxIn0.IRIQRYWFS5iymKJz_n3ri3-qYIv2jZC5OCwO6JgFd_bnE3dvxpQIpEvpngkwPjEUWutnMrl8wLkgCduQxZwMDIHIVXM3mi7chWs0aNB2TvPIYyl8CjeY2xb2wbhkyA6AZVUAYix1idsbo9_CYwZ71hcHBuRNWgvQQQ4nB9cMdougc-8QjhkBFy6scx-aTva1zSnbJ6nDLV-xESUvxEjlrgX91av3wnO6mrmOsfiUTA14xu_9VTd6k-dEpvuPmRhzJ3DdZNsPXB_DZpyAj6AlusrNmlelzr8L_itHFUqiVjmx26AkuKyYSy4q5tYSzPRwQ-XaKc6kDCaPZLnXwNEoQQ" 
-    }
-      
-    })
-      .then((response) => response.json())
-      .then((json) => {console.log(json),
-      this.deals = json})
+      fetch('https://vovachegotov.amocrm.ru/api/v4/leads',{
+        headers: {
+          'Authorization: Bearer ': "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImI1NTQ2MGNiNzNlNjllNmI0MzIwMWRhZTIyN2ZkYThmNDY1MzNkZjc3YWYyOGExZThjNjEwOGE2NGFmMWEyZGQ3ZDhmMTEyNzJmZTY4ODQ3In0.eyJhdWQiOiI0ZmE3M2VhNi1jMDdlLTQ3OTEtODk4MC1iNzYxNjVmODkyMGMiLCJqdGkiOiJiNTU0NjBjYjczZTY5ZTZiNDMyMDFkYWUyMjdmZGE4ZjQ2NTMzZGY3N2FmMjhhMWU4YzYxMDhhNjRhZjFhMmRkN2Q4ZjExMjcyZmU2ODg0NyIsImlhdCI6MTcwODM1Njk5NiwibmJmIjoxNzA4MzU2OTk2LCJleHAiOjE3MDg0NDMzOTYsInN1YiI6IjEwNjc5NDk0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNTcxODE0LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMWJmNWRlZGEtNzdmOS00YWJlLWJhMTQtOTVlMzRmZjdhYzVjIn0.o4QGpHohAOVtGMJdqwaKq8rFb_jTtT3VVjSmVyTjM4w0z-2WCwQzuc8tZiWzaJtUp0j5hGLu8fZqSJ_ao8LMub1yukBguSHKIEj1SaXCLwlLD6QNMiQcmGpGK202laC4mkidQf115n44-je_tyjXi_oTL71zKrOLvX0HGnQShkYtSh3SF00IPnOuuZb2y-yffQdSpaePBP-letfc00M4Q4u8RS3w-gSTBmEStPpkNAh1RphfIFY1ai2wApCCcizWLes7mV1IedN2CD97qlxs69CkHojaRfK_od9YUx_itEg8TuQwRaZDNWN2_G4AWpfcFMCUBTZZwq0b90zJtVxQvg"
+        }
+      })
+      .then(response => response.json())
+      .then(json => {console.log(json)})
       .catch(() => {
-        alert("Server error");
-      });
-  },
-};
+        alert('Server error')
+      })
+  }
+}
 </script>
 
 <style>
